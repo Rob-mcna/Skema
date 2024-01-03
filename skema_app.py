455ac5be3941039e495a778ac172d01a8291eb50
Rob-mcna <155495645+Rob-mcna@users.noreply.github.com>
2024-01-03 21:20:43 +0100

    skema_app.py

--- skema_app.py
+++ skema_app.py
@@ -11,8 +11,20 @@ import streamlit as st
 
 st.write('Hello World')
 
+import os
+
+# Everything is accessible via the st.secrets dict:
+st.write("DB username:", st.secrets["db_username"])
+st.write("DB password:", st.secrets["db_password"])
+st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
+
+# And the root-level secrets are also accessible as environment variables:
+st.write(
+    "Has environment variables been set:",
+    os.environ["db_username"] == st.secrets["db_username"],
+)
 # Verbose version
-my_db.connect(username=st.secrets.db_credentials.username, password=st.secrets.db_credentials.password)
+#my_db.connect(username=st.secrets.db_credentials.username, password=st.secrets.db_credentials.password)
 
 # Far more compact version!
-my_db.connect(**st.secrets.db_credentials)
+#my_db.connect(**st.secrets.db_credentials)
