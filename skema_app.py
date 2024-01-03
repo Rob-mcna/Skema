9ace83cd763997da9037e8511aff15f0bdae1196
Rob-mcna <155495645+Rob-mcna@users.noreply.github.com>
2024-01-03 22:30:08 +0100

    skema_app.py

--- skema_app.py
+++ skema_app.py
@@ -30,7 +30,7 @@ st.write(
 #my_db.connect(**st.secrets.db_credentials)
 # streamlit_app.py
 
-import SQLAlchemy as sql
+import sqlalchemy as sql
 
 # Create the SQL connection to pets_db as specified in your secrets file.
 conn = st.connection('pets_db', type='sql')
