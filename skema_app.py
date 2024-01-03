a4e113ec8f8ca60c77c7dbd9e4e2614a18ef4e40
Rob-mcna <155495645+Rob-mcna@users.noreply.github.com>
2024-01-03 23:03:26 +0100

    skema_app.py

--- skema_app.py
+++ skema_app.py
@@ -33,7 +33,7 @@ st.write(
 
 # Create the SQL connection to pets_db as specified in your secrets file.
 
-pip install matplotlib
+apt-get install python3-matplotlib
 
 conn = st.connection('pets_db', type='sql')
 
